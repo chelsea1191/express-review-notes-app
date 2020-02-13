e9941d4bd543f8790bfa19fea0667fb2dfcb439c
Chelsea Kramer <chelsea.kramer99@gmail.com>
2020-02-12 20:27:09 -0500

    initial

--- server.js
+++ server.js
@@ -1,10 +1,71 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
+const bodyParser = require("body-parser");
+const db = require("./db");
+let uuid = require("uuid/v4");
 
-app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
+const pathToNotes = path.join(__dirname, "notes.json");
+app.use(bodyParser.json());
+app.use(express.static("assets"));
+
+app.get("/", (req, res, next) =>
+  res.sendFile(path.join(__dirname, "index.html"))
+);
+
+app.get("/api/notes", (req, res, next) => {
+  db.readJSON(pathToNotes)
+    .then(notes => res.send(notes))
+    .catch(next);
+});
+
+app.post("/api/notes", (req, res, next) => {
+  db.readJSON(pathToNotes)
+    .then(notes => {
+      notes.push({
+        id: uuid(),
+        text: req.body.text,
+        archived: false
+      });
+      db.writeJSON(pathToNotes, notes);
+    })
+    .then(notes => res.send(notes))
+    .catch(next);
+});
+
+app.put("/api/notes/:id", (req, res, next) => {
+  const updated = req.body.archived;
+  const id = req.params.id;
+  db.readJSON(pathToNotes)
+    .then(notes => {
+      notes.forEach(note => {
+        if (note.id == id) {
+          note.archived = updated;
+        }
+      });
+
+      return notes;
+    })
+    .then(updatedNotes => {
+      db.writeJSON(pathToNotes, updatedNotes);
+    })
+    .then(notes => res.send(notes))
+    .catch(next);
+});
+
+app.delete("/api/notes/:id", (req, res, next) => {
+  const id = req.params.id;
+  db.readJSON(pathToNotes)
+    .then(notes => {
+      const remainingNotes = notes.filter(note => note.id != id);
+      return remainingNotes;
+    })
+    .then(leftoverNotes => {
+      return db.writeJSON(pathToNotes, leftoverNotes);
+    })
+    .then(() => res.status(204).send());
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, ()=> console.log(`listening on port ${port}`));
-
+app.listen(port, () => console.log(`listening on port ${port}`));
